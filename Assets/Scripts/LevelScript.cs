f4720f5033511afd4ebd25885eff86cb4a4b1567
StefanHohnwald <stefan.hohnwald@gmail.com>
2020-12-19 00:12:59 +0100

    Added automatic grid generation for misc object spawnpoints

--- Assets/Scripts/LevelScript.cs
+++ Assets/Scripts/LevelScript.cs
@@ -11,7 +11,8 @@ public class LevelScript : MonoBehaviour
     public List<GameObject> doorSpawnPoints;
     public List<GameObject> doorPrefabs;
     public List<GameObject> playerSpawnPoints;
-
+    public List<Vector2> thingSpawnPoints;
+    public Vector2 numThingSpawnPoints;
     private GameData gameData;
     void Start()
     {
@@ -34,12 +35,42 @@ public class LevelScript : MonoBehaviour
                 door.GetComponent<DoorScript>().doorDirection = dss.direction;
             }
         }
+
+        GameObject floorArea = GameObject.FindGameObjectWithTag("FloorArea");
+        if(floorArea)
+        {
+            Bounds bounds = floorArea.GetComponent<SpriteRenderer>().bounds;
+            Vector2 center = bounds.center;
+            Vector2 extends = bounds.extents;
+            for (int x = 1; x < numThingSpawnPoints.x; x++)
+            {
+                for (int y = 1; y < numThingSpawnPoints.y; y++)
+                {
+                    float offsetX = x * bounds.size.x / (numThingSpawnPoints.x);
+                    float offsetY = y * bounds.size.y / (numThingSpawnPoints.y);
+                    Vector2 point = new Vector2(center.x + offsetX - extends.x, center.y + offsetY - extends.y);
+                    this.thingSpawnPoints.Add(point);
+                }
+            }
+        }
+        /*
+        foreach(GameObject obj in thingSpawnPoints)
+        {
+            obj.transform.position += new Vector3(Random.Range(-1, 1), Random.Range(-1, 1));
+        }
+        */
+
         this.gameData.NotifyNewSceneLoaded(this);
     }
 
     // Update is called once per frame
     void Update()
     {
+        foreach(Vector2 vec in this.thingSpawnPoints)
+        {
+
+            Debug.DrawLine(vec, vec + Vector2.up * 0.2f, Color.green);
+        }
         
     }
 }
