ea135be8c2cf898016b9ee7f377e0f93dd5179f9
utkudagli <34172180+utkudagli@users.noreply.github.com>
2020-12-19 02:24:28 +0300

    Main Menu, Credits and Volume

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,31 +6,23 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     private Button newGame;
-    private Button options;
     private Button credits;
     private Button exit;
 
     private void Awake()
     {
         newGame = transform.Find("NewGame").GetComponent<Button>();
-        options = transform.Find("Options").GetComponent<Button>();
         credits = transform.Find("Credits").GetComponent<Button>();
         exit = transform.Find("Exit").GetComponent<Button>();
 
         newGame.onClick.AddListener(NewGame);
-        options.onClick.AddListener(Options);
         credits.onClick.AddListener(Credits);
         exit.onClick.AddListener(Exit);
     }
 
     private void NewGame()
     {
-        Application.LoadLevel("Scene1");
-    }
-
-    private void Options()
-    {
-        Application.LoadLevel("Options");
+        Application.LoadLevel("RandomRoomScene");
     }
 
     private void Credits()
