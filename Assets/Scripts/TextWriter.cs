b076734a5624634bc294c7263676b01b69ad61a2
utkudagli <34172180+utkudagli@users.noreply.github.com>
2020-12-19 00:54:10 +0300

    TextBox

---
+++ Assets/Scripts/TextWriter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriter : MonoBehaviour
+{
+    private Text uiText;
+    private string textToWrite;
+    private int charIndex;
+    private float timePerChar;
+    private float timer;
+    
+
+    // Update is called once per frame
+    private void Update(){
+        if (uiText != null)
+        {
+            timer -= Time.deltaTime;
+            if(timer <= 0f)
+            {
+                timer += timePerChar;
+                charIndex++;
+                uiText.text = textToWrite.Substring(0, charIndex);
+
+                if (charIndex >= textToWrite.Length)
+                {
+                    uiText = null;
+                    return;
+                }
+            }
+        }
+    }
+
+    public void AddWriter(Text uiText, string textToWrite, float timePerChar){
+        this.uiText = uiText;
+        this.textToWrite = textToWrite;
+        this.timePerChar = timePerChar;
+        charIndex = 0;
+    }
+}
---
+++ Assets/Scripts/UI_Assistant.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Assistant : MonoBehaviour
+{
+    [SerializeField] private TextWriter textWriter;
+    private Text messageText;
+    private int i = -1;
+    private Image messageBackground;
+    private Text toContinue;
+
+    private void Awake()
+    {
+        messageText = transform.Find("message").Find("Text").GetComponent<Text>();
+        messageBackground = transform.Find("message").Find("Image").GetComponent<Image>();
+        toContinue = transform.Find("message").Find("toContinue").GetComponent<Text>();
+        Application.targetFrameRate = 100;
+
+        string[] messageArr = new string[]
+        {
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            " Donec malesuada, nunc vel venenatis euismod, nibh elit consequat nisi, at tempus nisi libero a velit.",
+            " Vivamus lacinia lacinia massa, id mattis nisi feugiat in. Integer dictum auctor ultricies. Aenean quis est quis nisl ",
+            "feugiat fermentum a a tellus.",
+        };
+        InvokeTextBox(messageArr);
+    }
+    private void Start()
+    {
+        textWriter.AddWriter(messageText, "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+            " Donec malesuada, nunc vel venenatis euismod, nibh elit consequat nisi, at tempus nisi libero a velit." +
+            " Vivamus lacinia lacinia massa, id mattis nisi feugiat in. Integer dictum auctor ultricies. Aenean quis est quis nisl " +
+            "feugiat fermentum a a tellus.", .025f);
+    }
+    private void NextMsg(string[] messageArr)
+    {
+        i++;
+        if (i <= messageArr.Length)
+        {
+            if(i == messageArr.Length)
+            {
+                messageText.gameObject.SetActive(false);
+                messageBackground.gameObject.SetActive(false);
+                toContinue.gameObject.SetActive(false);
+            }
+            else
+            {
+                string msg = messageArr[i];
+                textWriter.AddWriter(messageText, msg, .025f);
+            }
+        }
+    }
+    private void InvokeTextBox(string[] messageArr)
+    {
+        Button getNextMsg = transform.Find("message").GetComponent<Button>();
+        getNextMsg.onClick.AddListener(delegate { NextMsg(messageArr); });
+    }
+}
