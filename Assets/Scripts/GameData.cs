e345c3c7096c1032053c6245e178fc6daf6c5fc5
StefanHohnwald <stefan.hohnwald@gmail.com>
2020-12-18 19:20:20 +0100

    Added persistent game data storage and player spawning

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : ScriptableObject
+{
+    static private GameData instance;
+
+    public GameObject playerCharacter;
+    public GameObject playerController;
+
+    bool bIsGameInitialized = false;
+
+    public void InitializeGame(LevelScript script)
+    {
+        if(bIsGameInitialized)
+        {
+            return;
+        }
+        bIsGameInitialized = true;
+        if (playerCharacter == null)
+        {
+            playerCharacter = Instantiate(script.playerCharacterPrefab, new Vector2(0, 0), Quaternion.identity);
+            DontDestroyOnLoad(playerCharacter);
+        }
+    }
+    public static GameData Get()
+    {
+        if (instance == null)
+        {
+            instance = ScriptableObject.CreateInstance<GameData>();
+        }
+        return instance;
+    }
+}
---
+++ Assets/Scripts/LevelScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject playerCharacterPrefab;
+
+    private GameData gameData;
+    void Start()
+    {
+        this.gameData = GameData.Get();
+        this.gameData.InitializeGame(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
