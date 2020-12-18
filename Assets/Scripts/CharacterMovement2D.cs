7bb01574ee922a5c8fc4716aeac676f656eb9ac0
StefanHohnwald <stefan.hohnwald@gmail.com>
2020-12-18 18:27:00 +0100

    Implemented basic movement, added base game level

---
+++ Assets/Scripts/CharacterMovement2D.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement2D : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float MovementSpeed = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FixedUpdate()
+    {
+        this.transform.Translate(this.inputVec * this.MovementSpeed * Time.fixedDeltaTime);
+    }
+
+    public virtual void ApplyMovementInput(Vector2 vec)
+    {
+        this.inputVec = vec;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject controlledObject;
+    CharacterMovement2D movement;
+    void Start()
+    {
+        if (!this.controlledObject)
+        {
+            return;
+        }
+        this.movement = this.controlledObject.GetComponent<CharacterMovement2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!movement)
+        {
+            return;
+        }
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        this.movement.ApplyMovementInput(input);
+    }
+}
