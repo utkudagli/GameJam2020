8811a004880736dd40e990a1873d5580314f9ad2
StefanHohnwald <stefan.hohnwald@gmail.com>
2020-12-18 19:32:37 +0100

    Added door overlaps and infinite level changes

---
+++ Assets/Scripts/DoorLevelChangeScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DoorLevelChangeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Ontriggerenter");
+        GameObject obj = collision.gameObject;
+        PlayerController pc = obj.GetComponent<PlayerController>();
+        if(pc)
+        {
+            obj.transform.position = new Vector2(0, 0);
+            SceneManager.LoadScene("RandomRoomScene");
+        }
+    }
+}
