a612b2059f869ef7c01cbe1855bd4c7479a045b4
utkudagli <34172180+utkudagli@users.noreply.github.com>
2020-12-19 15:04:14 +0300

    Volume slider fixed.

--- Assets/Scripts/SetVolume.cs
+++ Assets/Scripts/SetVolume.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SetVolume : MonoBehaviour
 {
     public AudioMixer mixer;
 
+    public void Start()
+    {
+        transform.GetComponent<Slider>().value = PlayerPrefs.GetFloat("OptionVolume");
+    }
+    public void Update()
+    {
+        float value = transform.GetComponent<Slider>().value;
+        PlayerPrefs.SetFloat("OptionVolume", value);
+    }
+
     public void SetLevel(float sliderValue)
     {
         mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }
+    
+    public void SetSliderValue(float value)
+    {
+        transform.GetComponent<Slider>().value = value;
     }
 }
