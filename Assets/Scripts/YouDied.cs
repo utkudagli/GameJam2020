c642f4fb9bb3edcf5ceabd9f17044c129c5b6e7e
utkudagli <34172180+utkudagli@users.noreply.github.com>
2020-12-19 22:57:44 +0300

    You Died

---
+++ Assets/Scripts/YouDied.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YouDied : MonoBehaviour
+{
+    private Button backToMM;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        backToMM = transform.Find("BacktoMainMenu").GetComponent<Button>();
+        backToMM.onClick.AddListener(BackToMainMenu);
+    }
+
+    private void BackToMainMenu()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+}
