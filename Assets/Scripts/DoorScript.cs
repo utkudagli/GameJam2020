9693dc3596718aa0d6705e948a7e01f946641042
StefanHohnwald <stefan.hohnwald@gmail.com>
2020-12-20 11:42:46 +0100

    Added randomized door sprites

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -6,7 +6,13 @@ public class DoorScript : MonoBehaviour
     // Start is called before the first frame update
 
     public EDirection doorDirection = EDirection.UP;
-
+    public List<Sprite> DoorSprites;
+    public SpriteRenderer myRenderer;
+    public void Start()
+    {
+        int index = Random.Range(0, DoorSprites.Count);
+        this.myRenderer.sprite = DoorSprites[index];
+    }
     // Update is called once per frame
     public void OnTriggerEnter2D(Collider2D collision)
     {
